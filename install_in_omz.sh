d5aa0e58650b0e3f6241143770d9bd658a91237e
davymai <davymai@qq.com>
2020-11-06 12:52:19 +0800

    Do custom install

---
+++ install_in_omz.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo "Creating symlink from here to your oh-my-zsh themes path..."
+
+mkdir -p ~/.oh-my-zsh/custom/themes/
+ln -f panda.zsh-theme ~/.oh-my-zsh/custom/themes/panda.zsh-theme
+
+echo "
+Done ! (the symlink is ~/.oh-my-zsh/custom/themes/panda.zsh-theme)
+----------------------------------------------------------------------------------
+Options:
+All options must be overridden in your .zshrc file.
+See README.md for more info.
+----------------------------------------------------------------------------------
+Requirements:
+Vim Panda patched font: See Panda for vim for more info.
+Z shell (zsh): See oh-my-zsh for more info.
+Make sure terminal is using 256-colors mode with export TERM=\"xterm-256color\"".
